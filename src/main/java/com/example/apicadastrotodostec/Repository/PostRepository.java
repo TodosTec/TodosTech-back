9a63b038e48cc986d10ea2847157fc4e72a5098f
henry-gois <night.xxxx12@gmail.com>
2023-10-10 09:39:26 -0300

    subindo alterações do post

--- src/main/java/com/example/apicadastrotodostec/Repository/PostRepository.java
+++ src/main/java/com/example/apicadastrotodostec/Repository/PostRepository.java
@@ -3,10 +3,19 @@ package com.example.apicadastrotodostec.Repository;
 import com.example.apicadastrotodostec.Entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query("SELECT p, u.cusername, u.cnome, u.clinksite FROM Post p, Usuario u WHERE u.clinksite IS NOT NULL AND u.ncdusuario = p.ncdusuario")
+    @Query("SELECT p, u.cusername, u.cnome, u.clinksite FROM Post p, Usuario u WHERE u.clinksite IS NULL AND u.ncdusuario = p.ncdusuario ORDER BY RANDOM() LIMIT 9")
     List<Post> findAllByLinkExistente();
-}// Trazer posts pagos
+
+    @Query("SELECT p, u.cusername, u.cnome, u.clinksite FROM Post p, Usuario u WHERE u.clinksite IS NOT NULL AND u.ncdusuario = p.ncdusuario ORDER BY RANDOM() LIMIT 1")
+    Post findOneByLinkExistente();
+
+    @Query(value = "SELECT p, u.clinksite FROM Post p, Usuario u  WHERE u.ncdusuario = p.ncdusuario AND u.clinksite IS NOT NULL ORDER BY RANDOM() LIMIT 1")
+    Optional<Post> findRandomPostsWithClinksite();
+
+}
