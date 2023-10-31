bc570a8ccd4f2570064d43282cdeb4e0d45ce7f7
henry-gois <night.xxxx12@gmail.com>
2023-10-31 15:40:42 -0300

    Comitando alterações de POST e excluíndo classe mensagem

--- src/main/java/com/example/apicadastrotodostec/Repository/MensagemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.apicadastrotodostec.Repository;
-
-import com.example.apicadastrotodostec.Entity.Mensagem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
-}
